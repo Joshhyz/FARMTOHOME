e283a66ae48f0ca901e3b230323a6e80e272f088
ryxchu <ryzabasas16@gmail.com>
2026-05-08 16:06:07 +0800

    update style to admin

--- admin.php
+++ admin.php
@@ -125,7 +125,7 @@ foreach ($orders as $order) {
 
 <title>FarmToHome Admin Panel</title>
 
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="admin.css">
 
 <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
